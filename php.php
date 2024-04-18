4c2aa51bfe7d2a5a4abcbd2d320de9f64d950c5c
fluffy29 <hmajzoub12@gmail.com>
2024-04-18 16:10:54 +0200

    Calculator with php

--- php.php
+++ php.php
@@ -1,8 +1,25 @@
-<?php
-echo __DIR__ . '<br>';
-echo __FILE__ . '<br>';
-echo __LINE__ . '<br>';
-echo __FUNCTION__ . '<br>';
-echo __CLASS__ . '<br>';
-echo __METHOD__ . '<br>';
-echo __NAMESPACE__ . '<br>';
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Calculator</title>
+</head>
+<body>
+    <form method="POST" action="">
+        X: <input type="text" name="x" placeholder="X">
+        +
+        Y: <input type="number" name="y" placeholder="Y">
+        <input type="submit" value="Calculate">
+    </form>
+
+    <?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $x = $_POST["x"];
+        $y = $_POST["y"];
+        $sum = $x + $y;
+        echo "the sum for this value is $x + $y = $sum";
+    }
+    ?>
+</body>
+</html>
