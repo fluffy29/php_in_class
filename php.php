386c49739794c709f0858cf14ba1e6f0aab0c673
fluffy29 <hmajzoub12@gmail.com>
2024-04-18 15:55:27 +0200

    Class if used inside a class the class name is returned. DIR is used to get the current directory. __DIR__ is used to get the directory of the file. __FILE__ is used to get the file name. __LINE__ is used to get the line number. __METHOD__ is used to get the method name. __FUNCTION__ is used to get the function name. __CLASS__ is used to get the class name. __TRAIT__ is used to get the trait name. __NAMESPACE__ is used to get the namespace name.

--- php.php
+++ php.php
@@ -1,7 +1,8 @@
 <?php
-define("LESSONS", [
-    "PHP",
-    "REACT",
-    "DATABASE"
-]);
-echo LESSONS[0];
+echo __DIR__ . '<br>';
+echo __FILE__ . '<br>';
+echo __LINE__ . '<br>';
+echo __FUNCTION__ . '<br>';
+echo __CLASS__ . '<br>';
+echo __METHOD__ . '<br>';
+echo __NAMESPACE__ . '<br>';
