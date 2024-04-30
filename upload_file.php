832d1bf5cd376131e73f0c4355156ece9095cd0d
fluffy29 <hmajzoub12@gmail.com>
2024-04-30 15:33:25 +0200

    Refactor upload_file.php to add action attribute to form

--- upload_file.php
+++ upload_file.php
@@ -3,7 +3,7 @@
 
     </head>
     <body>
-        <form method="post" enctype="multipart/form-data">
+        <form method="post" enctype="multipart/form-data" action="upload_file_action.php">
             <input type="file" name="myfile"/>
             <input type="submit" value = "loadfile"/>
         </form>
