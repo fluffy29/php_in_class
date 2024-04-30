df95335b854919633fd86fc095f507a695845a21
fluffy29 <hmajzoub12@gmail.com>
2024-04-30 15:48:05 +0200

    Fix newline at end of file issue in upload_file_action.php

--- upload_file_action.php
+++ upload_file_action.php
@@ -5,4 +5,7 @@ print_r($_FILES);
 echo '</pre>';
 
 
-$destination_path = 
+$destination_path = './image/' .$_FILES['myfile']['full_path'];
+move_uploaded_file($_FILES['myfile']['tmp_name'], $destination_path);
+
+?>
